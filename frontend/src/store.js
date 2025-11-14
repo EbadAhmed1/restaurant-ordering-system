f7f1c663668f937a88244735fe137ac622f6f4e4
EbadAhmed1 <ebadahmed20005@gmail.com>
2025-11-14 15:13:13 +0500

    configure Redux store with auth reducer

---
+++ frontend/src/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './features/auth/authSlice';
+// import cartReducer from './features/cart/cartSlice';
+// import menuReducer from './features/menu/menuSlice';
+
+const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        // cart: cartReducer, 
+        // menu: menuReducer, 
+    },
+});
+
+export default store;
