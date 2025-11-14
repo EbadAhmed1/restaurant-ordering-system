0585e96bb2e2e4cc71e7a3b60bc1ccd146e4dd4e
EbadAhmed1 <ebadahmed20005@gmail.com>
2025-11-14 15:50:36 +0500

    configure app entry point with Redux and routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import { ToastContainer } from 'react-toastify';
+import store from './store';
+import App from './App.jsx';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+// Import necessary CSS for Bootstrap and Toastify
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router>
+                <App />
+                <ToastContainer position="top-center" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
+            </Router>
+        </Provider>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
