7dd7fa8a3decf13ca8924ebf967fb6191343c9a0
EbadAhmed1 <ebadahmed20005@gmail.com>
2025-11-18 23:54:57 +0500

    integrate cartReducer into Redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlice';
-// import cartReducer from './features/cart/cartSlice';
-// import menuReducer from './features/menu/menuSlice';
+import cartReducer from './features/cart/cartSlice';
+import menuReducer from './features/menu/menuSlice';
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
-        // cart: cartReducer, 
-        // menu: menuReducer, 
+        cart: cartReducer, 
+        menu: menuReducer, 
     },
 });
 
